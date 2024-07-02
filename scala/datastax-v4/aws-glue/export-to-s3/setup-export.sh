817487626019d650bce1cf39111b67272a50831d
Maciej Korzeniowski <maciej.korzeniowski@sky.uk>
2024-07-02 13:34:42 +0000

    Adaptations to get AWS Glue up and running in Sky OVP Dev

--- scala/datastax-v4/aws-glue/export-to-s3/setup-export.sh
+++ scala/datastax-v4/aws-glue/export-to-s3/setup-export.sh
@@ -15,6 +15,10 @@ KEYSPACE_NAME=${3:-mykeyspace}
 TABLE_NAME=${4:-mytable}
 FORMAT=${6:-parquet}
 
+echo "Deleting CloudFormation ${STACK_NAME} to make way for fresh config."
+aws cloudformation delete-stack --stack-name ${STACK_NAME}
+# TODO - add a wait for delete state
+
 echo "Parent stack used: ${PARENT_STACK_NAME}"
 echo "Stack name used:   ${STACK_NAME}"
 echo "Keyspace used used: ${KEYSPACE_NAME}"
@@ -26,7 +30,8 @@ if ! command -v aws &> /dev/null; then
     exit 1
 fi
 
-export KEYSPACES_GLUE_BUCKET=$(aws cloudformation describe-stacks --query "Stacks[?StackName==\`$PARENT_STACK_NAME\`][].Outputs[?ExportName==\`KeyspacesBucketNameExport-$PARENT_STACK_NAME\`]".OutputValue --output text)
+#export KEYSPACES_GLUE_BUCKET=$(aws cloudformation describe-stacks --query "Stacks[?StackName==\`$PARENT_STACK_NAME\`][].Outputs[?ExportName==\`KeyspacesBucketNameExport-$PARENT_STACK_NAME\`]".OutputValue --output text)
+export KEYSPACES_GLUE_BUCKET=amazon-keyspaces-glue-eu-central-1-gluekscfstack-575545957491
 
 if [ -z "${KEYSPACES_GLUE_BUCKET}" ]; then
 	echo "Parent stack not found. Cloudformation Export not found KeyspacesBucketNameExport-$PARENT_STACK_NAME"
@@ -41,7 +46,7 @@ echo "Moving script to bucket ${KEYSPACES_GLUE_BUCKET}"
 
 aws s3api put-object --bucket $KEYSPACES_GLUE_BUCKET --key scripts/export-sample.scala --body export-sample.scala || exit 1
 
-aws cloudformation create-stack --stack-name ${STACK_NAME} --parameters ParameterKey=ParentStack,ParameterValue=$PARENT_STACK_NAME ParameterKey=KeyspaceName,ParameterValue=$KEYSPACE_NAME ParameterKey=TableName,ParameterValue=$TABLE_NAME ParameterKey=S3URI,ParameterValue=${S3URI} ParameterKey=FORMAT,ParameterValue=$FORMAT --template-body 'file://glue-job-export-to-s3.yaml' || exit 1  #--debug  
+aws cloudformation create-stack --stack-name ${STACK_NAME} --parameters ParameterKey=KeyspaceName,ParameterValue=$KEYSPACE_NAME ParameterKey=TableName,ParameterValue=$TABLE_NAME ParameterKey=S3URI,ParameterValue=${S3URI} ParameterKey=FORMAT,ParameterValue=$FORMAT --template-body 'file://glue-job-export-to-s3.yaml' || exit 1  #--debug  
 
 echo Waiting for CloudFormation stack to complete ...
 aws cloudformation wait stack-create-complete --stack-name ${STACK_NAME}  || exit 1 
